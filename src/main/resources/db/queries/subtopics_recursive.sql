e70b4b6af5ef857cb24e1e07b04c7ddf25b1a08b
Ove Gram Nipen <ove@nipen.no>
2017-01-27 13:58:44 +0100

    draft of recursive query

---
+++ src/main/resources/db/queries/subtopics_recursive.sql
@@ -0,0 +1,29 @@
+WITH RECURSIVE tree (topic_id, public_id, name, parent_id, is_primary, level) AS (
+  SELECT
+    t.id,
+    t.public_id,
+    t.name,
+    s.topic_id parent_id,
+    FALSE AS   is_primary,
+    0     AS   level
+  FROM
+    topic t
+    LEFT OUTER JOIN topic_subtopic s ON t.id = s.subtopic_id
+  WHERE t.public_id = ?
+
+  UNION ALL
+
+  SELECT
+    t.id,
+    t.public_id,
+    t.name,
+    s.topic_id parent_id,
+    s.is_primary,
+    level + 1
+  FROM
+    topic t
+    LEFT OUTER JOIN topic_subtopic s ON t.id = s.subtopic_id
+    INNER JOIN tree tr ON tr.topic_id = s.topic_id
+)
+SELECT *
+FROM tree;
