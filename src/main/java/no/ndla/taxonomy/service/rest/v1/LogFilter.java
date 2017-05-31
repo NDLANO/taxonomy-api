02f0ffe2efb7aab09b298ad3d05670fd6bc73c75
Ove Gram Nipen <ove@nipen.no>
2017-06-01 00:08:36 +0200

    improve logging

--- src/main/java/no/ndla/taxonomy/service/rest/v1/LogFilter.java
+++ src/main/java/no/ndla/taxonomy/service/rest/v1/LogFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 import static java.lang.System.currentTimeMillis;
 
@@ -17,33 +18,72 @@ public class LogFilter implements Filter {
 
     Logger logger = LoggerFactory.getLogger("accesslog");
 
-
-    public LogFilter() {
-    }
-
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        if (!logger.isInfoEnabled()) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        try {
+            doFilterWithLogging((HttpServletRequest)servletRequest, (HttpServletResponse)servletResponse, filterChain);
+        } finally {
+            MDC.clear();
+        }
+    }
+
+    private void doFilterWithLogging(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        try {
+            MDC.put("remote_addr", request.getRemoteAddr());
+            MDC.put("request_url", String.valueOf(request.getRequestURL()));
+            MDC.put("x-consumer-username", request.getHeader("x-consumer-username"));
+            MDC.put("x-consumer-id", request.getHeader("x-consumer-id"));
+            MDC.put("x-correlation-id", request.getHeader("x-correlation-id"));
+        } catch (Exception e) {
+            //Don't fail the user request if the log system is broken
+            e.printStackTrace();
+        }
 
         long starts = currentTimeMillis();
-        filterChain.doFilter(servletRequest, servletResponse);
-        long ends = currentTimeMillis();
+        filterChain.doFilter(request, response);
 
-        MDC.put("remote_addr", request.getRemoteAddr());
-        MDC.put("completion_time", String.valueOf(ends - starts));
-        MDC.put("http_status", String.valueOf(response.getStatus()));
-        MDC.put("request_url", String.valueOf(request.getRequestURL()));
+        try {
+            MDC.put("completion_time", String.valueOf(currentTimeMillis() - starts));
+            MDC.put("http_status", String.valueOf(response.getStatus()));
 
-        if (response.getStatus() == HttpServletResponse.SC_CREATED) {
-            MDC.put("path_location", response.getHeader("Location"));
+            if (logger.isDebugEnabled()) {
+                String headers = extractHeaders(request);
+                MDC.put("headers", headers);
+            }
+
+            if (response.getStatus() == HttpServletResponse.SC_CREATED) {
+                MDC.put("path_location", response.getHeader("Location"));
+            }
+
+            logger.info(request.getMethod() + " " + request.getRequestURL());
+        } catch (Exception e) {
+            //Don't fail the user request if the log system is broken
+            e.printStackTrace();
         }
-        logger.info(request.getMethod() + " " + request.getRequestURL());
+    }
+
+    private String extractHeaders(HttpServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            Enumeration<String> headers = request.getHeaders(headerName);
+            while (headers.hasMoreElements()) {
+                String value = headers.nextElement();
+                sb.append(headerName).append(": ").append(value).append("\n");
+            }
+        }
+        return sb.toString();
     }
 
     @Override
