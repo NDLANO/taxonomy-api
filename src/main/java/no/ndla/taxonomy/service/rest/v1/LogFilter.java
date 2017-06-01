537e5439008b456257313ef3766e22202ae962a9
Siv Midtun Hollup <siv.hollup@gmail.com>
2017-06-01 23:42:16 +0200

    Extract data from jwt

--- src/main/java/no/ndla/taxonomy/service/rest/v1/LogFilter.java
+++ src/main/java/no/ndla/taxonomy/service/rest/v1/LogFilter.java
@@ -1,5 +1,9 @@
 package no.ndla.taxonomy.service.rest.v1;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.apache.commons.codec.binary.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -9,7 +13,9 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.Enumeration;
+import java.util.Map;
 
 import static java.lang.System.currentTimeMillis;
 
@@ -30,7 +36,7 @@ public class LogFilter implements Filter {
         }
 
         try {
-            doFilterWithLogging((HttpServletRequest)servletRequest, (HttpServletResponse)servletResponse, filterChain);
+            doFilterWithLogging((HttpServletRequest) servletRequest, (HttpServletResponse) servletResponse, filterChain);
         } finally {
             MDC.clear();
         }
@@ -43,6 +49,25 @@ public class LogFilter implements Filter {
             MDC.put("x-consumer-username", request.getHeader("x-consumer-username"));
             MDC.put("x-consumer-id", request.getHeader("x-consumer-id"));
             MDC.put("x-correlation-id", request.getHeader("x-correlation-id"));
+
+            //Should be in doFilter, we always want this info
+            String authorizationHeader = request.getHeader("authorization");
+            DecodedJWT jwt = JWT.decode(authorizationHeader.substring(6));
+            MDC.put("JWT-signature", jwt.getSignature());
+            MDC.put("JWT-iat", jwt.getIssuedAt().toString());
+            MDC.put("JWT-exp", jwt.getExpiresAt().toString());
+            MDC.put("JWT-iss", jwt.getIssuer());
+            Map<String, Claim> claims = jwt.getClaims();
+            System.out.println("claims: " + claims.keySet().toString());
+            Claim app_metadata = claims.get("app_metadata");
+            Map<String, Object> appMetadataMap = app_metadata.asMap();
+            System.out.println("metadata keys: " + appMetadataMap.keySet().toString());
+            Object roles = appMetadataMap.get("roles");
+            System.out.println("roles: " + roles.toString());
+            MDC.put("roles", roles.toString());
+            String decoded = StringUtils.newStringUtf8(Base64.getDecoder().decode(jwt.getPayload()));
+            MDC.put("JWT-payload", decoded);
+
         } catch (Exception e) {
             //Don't fail the user request if the log system is broken
             e.printStackTrace();
