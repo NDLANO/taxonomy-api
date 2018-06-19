80a96861ee52887f027b173d36f8ba211c6e43ae
Øystein Øvrebø <oystein.ovrebo@bouvet.no>
2018-06-19 12:30:06 +0200

    NDLA-132 Virker, men trenger litt forfining

---
+++ src/main/java/no/ndla/taxonomy/service/SubjectTopicRankUpdater.java
@@ -0,0 +1,18 @@
+package no.ndla.taxonomy.service;
+
+import no.ndla.taxonomy.service.domain.SubjectTopic;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class SubjectTopicRankUpdater {
+
+
+    public List<SubjectTopic> rank(List<SubjectTopic> existing, SubjectTopic topicToRank, int desiredRank){
+        return existing;
+    }
+
+
+}
