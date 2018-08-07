3f5353733353faf2a6afb7a79449cff82e138e23
kjetilhamre <kjetil.hamre@bouvet.no>
2018-08-07 15:31:14 +0200

    NDLA-137 optimizations and fixes

--- src/main/java/no/ndla/taxonomy/WebSecurityConfig.java
+++ src/main/java/no/ndla/taxonomy/WebSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
 @Profile("prod")
 @Configuration
@@ -28,8 +27,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf()
                 .disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(authFilter, AnonymousAuthenticationFilter.class);
-        http.addFilterBefore(logFilter,  AuthFilter.class);
-
     }
 }
