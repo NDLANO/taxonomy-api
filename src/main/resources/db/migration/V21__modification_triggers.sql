dd75c57b74c79fc9934adf314e48addf1616e93c
Øystein Øvrebø <oystein.ovrebo@bouvet.no>
2018-08-09 13:54:30 +0200

    NLDA-109 Databasemigrering med triggere som lagrer siste modifiserings-tidspunkt for hver ressurs

---
+++ src/main/resources/db/migration/V21__modification_triggers.sql
@@ -0,0 +1,110 @@
+drop function if exists updatelastmodified() cascade;
+
+CREATE OR REPLACE FUNCTION updatelastmodified()
+  RETURNS trigger LANGUAGE plpgsql
+AS
+$function$
+begin
+  insert into public.freshness (resource, last_modified)
+  values (TG_TABLE_NAME, now())
+  on conflict (resource) do update set last_modified = now();
+  return NEW;
+end;
+$function$
+;
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on filter
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on filter_translation
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on relevance
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on relevance_translation
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on resource
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on resource_filter
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on resource_resource_type
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on resource_translation
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on resource_type
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on resource_type_translation
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on subject
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on subject_topic
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on subject_translation
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on topic
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on topic_filter
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on topic_resource
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on topic_subtopic
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on topic_translation
+execute procedure updatelastmodified();
+
+create trigger update_freshness
+  after insert or update or delete or truncate
+  on url_map
+execute procedure updatelastmodified();
+
