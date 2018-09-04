5ea74152446ccd51cc6dcaedfc41f05bf789ac1a
Øystein Øvrebø <oystein.ovrebo@bouvet.no>
2018-09-04 14:59:27 +0200

    NDLA-145 utvidet til å også støtte prefiksene "printpdf", "easyreader", "h5p/embed", "h5pcontent" - suffiksene "/oembed" og "download", og andre parametre enn "fag"

--- src/main/java/no/ndla/taxonomy/service/OldUrlCanonifier.java
+++ src/main/java/no/ndla/taxonomy/service/OldUrlCanonifier.java
@@ -1,12 +1,18 @@
 package no.ndla.taxonomy.service;
 
 /**
- * Used to ensure that queries for old style URLS of various kinds (ndla.no/node/xxx) are transformed to the same format
+ * Used to ensure that queries for old style URLS of various kinds (ndla.no/node/xxx) are transformed to the same format,
+ * used in lookup.
  */
 public class OldUrlCanonifier {
 
-    public String canonify(String oldUrl) {
-        oldUrl = discardMenuVariations(oldUrl);
+    private static final String[] KNOWN_NODE_PREFIXES = new String[]{"printpdf", "easyreader", "h5p/embed", "h5pcontent"};
+    private static final String[] KNOWN_NODE_SUFFIXES = new String[]{"/menu", "/oembed", "/download"};
+
+
+    String canonify(String oldUrl) {
+        oldUrl = replaceKnownNodePrefixes(oldUrl);
+        oldUrl = discardKnownNodeSuffixes(oldUrl);
         String nodeId = "";
         String fagId = "";
         for (String token : tokenize(oldUrl)) {
@@ -14,26 +20,42 @@ public class OldUrlCanonifier {
                 int nodeStartsAt = findNodeStartsAt(token);
                 nodeId = token.substring(nodeStartsAt);
             } else if (token.contains("fag=")) {
-                fagId = "?" + token.substring(token.indexOf("fag="));
+                int start = token.indexOf("fag=");
+                int ampersandIndex = token.indexOf("&");
+                int end = ampersandIndex > start ? ampersandIndex: token.length();
+                fagId = "?" + token.substring(start, end);
             }
         }
         return "ndla.no" + nodeId + fagId;
     }
 
-    private String discardMenuVariations(String oldUrl) {
-        if (oldUrl.contains("/menu")) {
-            int start = oldUrl.indexOf("/menu");
-            int indexOfSlashAfter = oldUrl.indexOf("/", start + 1);
-            int indexOfQuestionMark = oldUrl.indexOf("?", start);
-            String partToRemove;
-            if (indexOfSlashAfter != -1) {
-                partToRemove = oldUrl.substring(start, indexOfSlashAfter + 1);
-            } else if (indexOfQuestionMark != -1) {
-                partToRemove = oldUrl.substring(start, indexOfQuestionMark);
-            } else partToRemove = "/menu";
-            return oldUrl.replace(partToRemove, "");
-        } else return oldUrl;
+    private String discardKnownNodeSuffixes(String oldUrl) {
+        for (String suffix : KNOWN_NODE_SUFFIXES) {
+            if (oldUrl.contains(suffix)) {
+                int start = oldUrl.indexOf(suffix);
+                int indexOfSlashAfter = oldUrl.indexOf("/", start + 1);
+                int indexOfQuestionMark = oldUrl.indexOf("?", start);
+                String partToRemove;
+                if (indexOfSlashAfter != -1) {
+                    partToRemove = oldUrl.substring(start, indexOfSlashAfter + 1);
+                } else if (indexOfQuestionMark != -1) {
+                    partToRemove = oldUrl.substring(start, indexOfQuestionMark);
+                } else partToRemove = suffix;
+                return oldUrl.replace(partToRemove, "");
+            }
+        }
+        return oldUrl;
     }
+
+    private String replaceKnownNodePrefixes(String oldUrl) {
+        for (String prefix : KNOWN_NODE_PREFIXES) {
+            if (oldUrl.contains(prefix)) {
+                return oldUrl.replace(prefix, "node");
+            }
+        }
+        return oldUrl;
+    }
+
 
     private int findNodeStartsAt(String token) {
         return token.substring(0, token.lastIndexOf("/")).lastIndexOf("/");
