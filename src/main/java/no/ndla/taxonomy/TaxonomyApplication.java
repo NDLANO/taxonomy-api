3d1b9127e0d8108b99cd899259b3568f4d09de15
Øystein Øvrebø <oystein.ovrebo@bouvet.no>
2019-02-05 16:00:15 +0100

    NDLA-109 simplified version that directly refreshes view from triggers (fixes testing problems)

--- src/main/java/no/ndla/taxonomy/TaxonomyApplication.java
+++ src/main/java/no/ndla/taxonomy/TaxonomyApplication.java
@@ -1,12 +1,10 @@
 package no.ndla.taxonomy;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
-@EnableScheduling
 public class TaxonomyApplication {
     public static void main(String[] args) throws InterruptedException {
         run(TaxonomyApplication.class, args);
--- src/main/resources/db/migration/V23__modification_triggers.sql
+++ src/main/resources/db/migration/V23__modification_triggers.sql
@@ -1,118 +1,45 @@
-CREATE TABLE freshness (
-  resource VARCHAR(128) NOT NULL,
-  last_modified TIMESTAMP NOT NULL
-);
+drop function if exists update_url_cache() cascade;
 
-CREATE UNIQUE INDEX freshness_idx
-  ON freshness (resource);
-
-drop function if exists updatelastmodified() cascade;
-
-CREATE OR REPLACE FUNCTION updatelastmodified()
+CREATE OR REPLACE FUNCTION update_url_cache()
   RETURNS trigger LANGUAGE plpgsql
 AS
 $function$
 begin
-  insert into public.freshness (resource, last_modified)
-  values (TG_TABLE_NAME, now())
-  on conflict (resource) do update set last_modified = now();
-  return NEW;
+  REFRESH MATERIALIZED VIEW CONCURRENTLY cached_url;
+  RETURN NULL;
 end;
 $function$
 ;
 
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on filter
-execute procedure updatelastmodified();
-
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on filter_translation
-execute procedure updatelastmodified();
-
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on relevance
-execute procedure updatelastmodified();
-
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on relevance_translation
-execute procedure updatelastmodified();
-
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on resource
-execute procedure updatelastmodified();
-
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on resource_filter
-execute procedure updatelastmodified();
-
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on resource_resource_type
-execute procedure updatelastmodified();
-
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on resource_translation
-execute procedure updatelastmodified();
-
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on resource_type
-execute procedure updatelastmodified();
-
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on resource_type_translation
-execute procedure updatelastmodified();
-
-create trigger update_freshness
+create trigger refresh_paths
   after insert or update or delete or truncate
   on subject
-execute procedure updatelastmodified();
+execute procedure update_url_cache();
 
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on subject_topic
-execute procedure updatelastmodified();
-
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on subject_translation
-execute procedure updatelastmodified();
-
-create trigger update_freshness
+create trigger refresh_paths
   after insert or update or delete or truncate
   on topic
-execute procedure updatelastmodified();
+execute procedure update_url_cache();
 
-create trigger update_freshness
+create trigger refresh_paths
   after insert or update or delete or truncate
-  on topic_filter
-execute procedure updatelastmodified();
+  on resource
+execute procedure update_url_cache();
 
-create trigger update_freshness
+create trigger refresh_paths
   after insert or update or delete or truncate
-  on topic_resource
-execute procedure updatelastmodified();
+  on subject_topic
+execute procedure update_url_cache();
 
-create trigger update_freshness
+create trigger refresh_paths
   after insert or update or delete or truncate
   on topic_subtopic
-execute procedure updatelastmodified();
+execute procedure update_url_cache();
 
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on topic_translation
-execute procedure updatelastmodified();
+create trigger refresh_paths
+after insert or update or delete or truncate
+on topic_resource
+execute procedure update_url_cache();
 
-create trigger update_freshness
-  after insert or update or delete or truncate
-  on url_map
-execute procedure updatelastmodified();
+
 
