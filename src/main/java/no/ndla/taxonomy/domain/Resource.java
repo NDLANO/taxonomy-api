9793d09d8fdf07bd118c09f9896b2659b0e5a5ad
Gunnar Velle <gunnar.velle@gmail.com>
2022-05-06 09:26:19 +0200

    Update relevance for connections.

--- src/main/java/no/ndla/taxonomy/domain/Resource.java
+++ src/main/java/no/ndla/taxonomy/domain/Resource.java
@@ -15,7 +15,7 @@ import java.net.URI;
 import java.util.*;
 import java.util.stream.Collectors;
 
-@NamedEntityGraph(name = Resource.GRAPH, attributeNodes = { @NamedAttributeNode(value = "metadata"),
+@NamedEntityGraph(name = Resource.GRAPH, includeAllAttributes = true, attributeNodes = { @NamedAttributeNode(value = "metadata"),
         @NamedAttributeNode(value = "resourceTranslations"),
         @NamedAttributeNode(value = "resourceResourceTypes", subgraph = "resourceResourceTypes"),
         @NamedAttributeNode(value = "nodes", subgraph = "node-with-connections") }, subgraphs = {
