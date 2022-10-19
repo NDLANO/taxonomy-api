df3476846f505933d6dca4ad20c21cd7b68a944e
Gunnar Velle <gunnar.velle@gmail.com>
2022-10-19 13:23:28 +0200

    Adds metadata to connection sql. Adds some indexes to improve fetch speed Forgot to return metadata for node_resource

--- src/main/java/no/ndla/taxonomy/repositories/NodeRepository.java
+++ src/main/java/no/ndla/taxonomy/repositories/NodeRepository.java
@@ -16,22 +16,22 @@ import java.util.List;
 import java.util.Optional;
 
 public interface NodeRepository extends TaxonomyRepository<Node> {
-    @Query("SELECT DISTINCT n FROM Node n LEFT JOIN FETCH n.cachedPaths LEFT JOIN FETCH n.metadata m"
-            + " LEFT JOIN FETCH m.grepCodes LEFT JOIN FETCH m.customFieldValues cvf LEFT JOIN cvf.customField"
-            + " LEFT JOIN FETCH n.translations WHERE n.context = :context")
+    @Query("SELECT DISTINCT n FROM Node n LEFT JOIN n.cachedPaths JOIN FETCH n.metadata m"
+            + " LEFT JOIN m.grepCodes LEFT JOIN FETCH m.customFieldValues cvf LEFT JOIN cvf.customField"
+            + " LEFT JOIN n.translations WHERE n.context = :context")
     List<Node> findAllByContextIncludingCachedUrlsAndTranslations(boolean context);
 
-    @Query("SELECT DISTINCT n FROM Node n LEFT JOIN FETCH n.cachedPaths LEFT JOIN FETCH n.metadata m"
-            + " LEFT JOIN FETCH m.grepCodes LEFT JOIN FETCH m.customFieldValues cvf LEFT JOIN cvf.customField"
-            + " LEFT JOIN FETCH n.translations WHERE n.publicId = :publicId")
+    @Query("SELECT DISTINCT n FROM Node n LEFT JOIN n.cachedPaths JOIN FETCH n.metadata m"
+            + " LEFT JOIN m.grepCodes LEFT JOIN FETCH m.customFieldValues cvf LEFT JOIN cvf.customField"
+            + " LEFT JOIN n.translations WHERE n.publicId = :publicId")
     Optional<Node> findFirstByPublicIdIncludingCachedUrlsAndTranslations(URI publicId);
 
-    @Query("SELECT DISTINCT n FROM Node n LEFT JOIN FETCH n.cachedPaths WHERE n.publicId = :publicId")
+    @Query("SELECT DISTINCT n FROM Node n LEFT JOIN n.cachedPaths WHERE n.publicId = :publicId")
     Optional<Node> findFirstByPublicIdIncludingCachedUrls(URI publicId);
 
     Optional<Node> findFirstByPublicId(URI publicId);
 
     @EntityGraph(value = Node.GRAPH, type = EntityGraph.EntityGraphType.LOAD)
-    @Query("SELECT DISTINCT n FROM Node n LEFT JOIN FETCH n.cachedPaths WHERE n.publicId = :publicId")
+    @Query("SELECT DISTINCT n FROM Node n LEFT JOIN n.cachedPaths WHERE n.publicId = :publicId")
     Optional<Node> fetchNodeGraphByPublicId(URI publicId);
 }
