c198470ed7f4d8f9d8a6dad79448592667b319f6
Gunnar Velle <gunnar.velle@gmail.com>
2022-10-31 10:30:07 +0100

    Does this fix strange test error?

--- src/main/java/no/ndla/taxonomy/config/LiquibaseConfig.java
+++ src/main/java/no/ndla/taxonomy/config/LiquibaseConfig.java
@@ -11,16 +11,13 @@ import liquibase.exception.LiquibaseException;
 import liquibase.integration.spring.SpringLiquibase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ResourceLoaderAware;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -46,7 +43,6 @@ public class LiquibaseConfig implements ResourceLoaderAware {
 
     private ResourceLoader resourceLoader;
 
-    @Async
     @EventListener
     public void onApplicationReady(ApplicationReadyEvent event) throws Exception {
         try {
