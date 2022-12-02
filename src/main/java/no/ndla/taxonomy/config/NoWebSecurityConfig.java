00f79774e034904435045f6a311f0a7640937700
Gunnar Velle <gunnar.velle@gmail.com>
2022-12-02 13:41:50 +0100

    Bump to boot 2.7.6 to prepare for 3.0.0

--- src/main/java/no/ndla/taxonomy/config/NoWebSecurityConfig.java
+++ src/main/java/no/ndla/taxonomy/config/NoWebSecurityConfig.java
@@ -7,20 +7,21 @@
 
 package no.ndla.taxonomy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @Profile("!auth")
-public class NoWebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class NoWebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
         http.headers().cacheControl().disable();
+        return http.build();
     }
 }
