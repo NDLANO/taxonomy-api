874a80b01fc825c613e20928186c5bf0501d6c36
Jonas Natten <jonas.natten@knowit.no>
2024-06-26 13:00:15 +0200

    Enable json logging in docker containers

--- run-app.sh
+++ run-app.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+# Export docker profile to enable json logging when running in docker
+if [[ "$RUNNING_IN_DOCKER" = "true" ]]; then
+  PROVIDED_PROFILES=${SPRING_PROFILES_ACTIVE}
+  if [[ -z "$PROVIDED_PROFILES" ]]; then
+    export SPRING_PROFILES_ACTIVE="docker"
+  else
+    export SPRING_PROFILES_ACTIVE="docker,$PROVIDED_PROFILES"
+  fi
+fi
+
 java -Xmx${HEAPSPACE_MAX:-"1000m"} --add-exports java.desktop/sun.font=ALL-UNNAMED -jar /app.jar
