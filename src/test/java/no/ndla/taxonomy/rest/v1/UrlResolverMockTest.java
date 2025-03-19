40e163acc9bc9bed705768f2714a86c7c5e93dc1
Gunnar Velle <gunnar.velle@gmail.com>
2025-03-19 14:48:09 +0100

    Bump minor boot version

--- src/test/java/no/ndla/taxonomy/rest/v1/UrlResolverMockTest.java
+++ src/test/java/no/ndla/taxonomy/rest/v1/UrlResolverMockTest.java
@@ -32,8 +32,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -47,7 +47,7 @@ import org.springframework.web.context.WebApplicationContext;
  * Test controller only
  */
 public class UrlResolverMockTest extends AbstractIntegrationTest {
-    @MockBean
+    @MockitoBean
     UrlResolverService urlResolverService;
 
     private MockMvc mvc;
