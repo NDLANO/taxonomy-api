81d8289f167fede49e41e44de156312fda1598e3
Gunnar Velle <gunnar.velle@gmail.com>
2025-11-06 07:34:02 +0100

    Dont generate root types for now

--- typescript/scripts/generate-openapi.ts
+++ typescript/scripts/generate-openapi.ts
@@ -23,26 +23,25 @@ async function generate_types(appName: string) {
   const jsonFile = `./openapi.json`;
   console.log(`Parsing ${jsonFile} to generate typescript files...`);
   const schema = await fs.promises.readFile(jsonFile, "utf8");
+  const schemaContent = JSON.parse(schema);
 
-  const ast = await openapiTS(JSON.parse(schema), {
+  const ast = await openapiTS(schemaContent, {
     exportType: true,
     generatePathParams: true,
-    rootTypes: true,
-    rootTypesNoSchemaPrefix: true,
     transform(schemaObject, meta) {
       if (
         schemaObject.type === "object" &&
         schemaObject.additionalProperties &&
         schemaObject.properties?.empty
       ) {
-          // Records generated from HashMaps have additional boolean property empty
+        // Records generated from HashMaps have additional boolean property empty
         delete schemaObject.properties;
       }
       return undefined;
     },
   });
 
-  const outputPath = `./taxonomy-api.ts`;
+  const outputPath = `./taxonomy-api-openapi.ts`;
   const output = astToString(ast);
 
   console.log(`Outputting to ${outputPath}`);
